11011765a65b57bcb8371114ac10fa8b73e3b8f4
shwns212 <shwns212@gmail.com>
2021-01-26 23:00:28 +0900

    kafka 테스트

--- shop-product/src/main/java/com/jun/shop/ShopProductApplication.java
+++ shop-product/src/main/java/com/jun/shop/ShopProductApplication.java
@@ -2,11 +2,18 @@ package com.jun.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+
+import com.jun.shop.kafka.KafkaRunner;
 
 @SpringBootApplication
+@EnableR2dbcRepositories
 public class ShopProductApplication {
 
 	public static void main(String[] args) {
+		new Thread(() ->  {
+			new KafkaRunner().run();
+		}).start();
 		SpringApplication.run(ShopProductApplication.class, args);
 	}
 
